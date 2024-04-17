2723f29b97636847542bf02787cb3878acb6c9f6
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2024-04-17 23:01:24 +0500

    Setup react-native app

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>My First React-Native App</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
